ff31d0a52712cbebb181d0ba34708501f259287b
daver234 <dave.rothschild@gmail.com>
2017-09-21 23:18:31 -0700

    - added CreateGoalVC and UI - connected outlets and actions

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,33 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by David Rothschild on 9/21/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
