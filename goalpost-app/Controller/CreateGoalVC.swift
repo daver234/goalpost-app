343a3d9c09c72d6d1cda89c8ac1a52adbfd68683
daver234 <dave.rothschild@gmail.com>
2017-09-22 12:21:06 -0700

    - implement UIView animation so that Next button binds to key board and animates up when tapping in text field to bring up iPhone keyboard - implement select/deselect color for short term and long term buttons on CreateGoalVC

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -14,14 +14,24 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
    
     }
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     @IBAction func longTermBtnPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
     }
     
     @IBAction func nextBtnWasPressed(_ sender: Any) {
