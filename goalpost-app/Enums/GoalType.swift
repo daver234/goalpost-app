4247bad050aef88ff86ba11a8e16f41cc06e5ed2
daver234 <dave.rothschild@gmail.com>
2017-09-21 22:17:30 -0700

    - add enum for goal type

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by David Rothschild on 9/21/17.
+//  Copyright © 2017 Dave Rothschild. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
